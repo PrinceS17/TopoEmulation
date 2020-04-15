2d929a49f60f0fd46bf8e5fb0cd60717b732fd7b
Jinhui Song <jinhuis2@illinois.edu>
2020-04-14 22:39:12 -0500

    Add delays.

--- set_iperf.py
+++ set_iperf.py
@@ -1,6 +1,7 @@
 import os, sys, time
 import subprocess as sp
 from multiprocessing import Process, Lock
+from time import sleep
 import shlex
 
 
@@ -14,7 +15,7 @@ def test_ls(path=None):
 
 def print_help():
     print('Usage:   python3 set_iperf.py -r N_MIN:N_MAX [-t DURATION] [-ctrl [-c CPORT_BASE]]')
-    print('                 [-a DES_ADDRESS] [-p PORT_BASE] [-s IPERF_FOLDER] [-out]')
+    print('                 [-a DES_ADDRESS] [-p PORT_BASE] [-s IPERF_FOLDER] [-out] [-asyn]')
     print('Note:    -slow   Enable time source.')
     exit(0)
 
@@ -25,12 +26,14 @@ def main():
             '-s':'', '-c':5000}
             # '/home/srcuser/Developer/iperf/src'
     cur_opt = None
-    enable_ctrl, enable_out = False, False
+    enable_ctrl, enable_out, asyn = False, False, False
     for arg in sys.argv[1:]:
         if arg == '-ctrl':
             enable_ctrl = True
         elif arg == '-out':
             enable_out = True
+        elif arg == '-asyn':
+            asyn = True
         elif arg in opts:
             cur_opt = arg
         elif cur_opt in opts:
@@ -47,7 +50,12 @@ def main():
     cport_base = int(opts['-c'])
     duration = int(opts['-t'])
     procs = []
-    for i in range(n_min, n_max + 1):
+    delay = 0
+    for i in range(n_min - 1, n_max):
+        if i == n_min  - 1:
+            i = n_max
+        elif i > n_min + 2:
+            delay = 30
         port = port_base + i
         des_ip = ip0[:5] + str(i) + ip0[6:]
         iperf_path = os.path.join(opts['-s'], 'iperf3') if opts['-s'] else 'iperf3'
@@ -56,11 +64,13 @@ def main():
         log_str = ' --logfile iperf-log-%s.txt' % i if enable_ctrl else ''
         cmd = '%s -c %s -p %s -t %s%s%s%s' \
             % (iperf_path, des_ip, port, duration, cport_str, log_str, out_str)
-        print(cmd)
+        print("%f s: %s" % (time.time(), cmd))
         # os.popen(cmd)
         # iargs = shlex.split(cmd)
         p = sp.Popen(cmd, shell=True)
         procs.append(p)
+        if asyn:
+            sleep(delay)		# sleep 3s to create different enter times
     for p in procs:
         p.wait()
 
